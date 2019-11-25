95ef0d11d815c81a0fdc86af2bf12180c7ca1fe5
Konrad Nölle <konrad@familie-noelle.de>
2019-11-25 02:18:56 +0100

    Fix status bar width

--- dwm.c
+++ dwm.c
@@ -936,7 +936,7 @@ drawbar(Monitor *m)
 	if ((w = m->ww - sw - stw - x) > bh) {
 		if (m->sel) {
 			drw_setscheme(drw, scheme[m == selmon ? SchemeSel : SchemeNorm]);
-			drw_text(drw, x, 0, w, bh, lrpad / 2, m->sel->name, 0);
+			drw_text(drw, x, 0, w + stw, bh, lrpad / 2, m->sel->name, 0);
 			if (m->sel->isfloating)
 				drw_rect(drw, x + boxs, boxs, boxw, boxw, m->sel->isfixed, 0);
 		} else {
